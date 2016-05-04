db3980a31899c681dac868586cb34b42008afcb0
Keith Moore <keith.m4@gmail.com>
2016-05-04 13:51:10 -0400

    added angular routes
    server.js uses npm connect and serve-static to get around x-origin
    request errors

--- javascripts/main.module.js
+++ javascripts/main.module.js
@@ -1,8 +1,21 @@
 var coffeeApp = angular.module('coffeeApp', ['ngRoute']);
+var apiUrl = 'http://localhost:3000';
 
 coffeeApp.config(function($routeProvider){
 	$routeProvider.when('/', {
 		templateUrl: 'pages/main.html',
+		controller: 'mainController'
+	});
+	$routeProvider.when('/register', {
+		templateUrl: 'pages/register.html',
+		controller: 'mainController'
+	});
+	$routeProvider.when('/login', {
+		templateUrl: 'pages/login.html',
+		controller: 'mainController'
+	});
+	$routeProvider.when('/options', {
+		templateUrl: 'pages/options.html',
 		controller: 'mainController'
 	});
 	$routeProvider.otherwise({
@@ -12,5 +25,12 @@ coffeeApp.config(function($routeProvider){
 
 
 coffeeApp.controller('mainController', function($scope, $http){	
-	console.log('we get signal');
+	$http({
+		method: 'GET',
+		url: apiUrl
+	}).then(function successCallback(response){
+		console.log(response);	
+	}, function errorCallback(status){
+		console.log(status);
+	});
 });
